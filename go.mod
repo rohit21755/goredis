8289543bad67fb7eca8f68bd53ce586b2204d657
rohit21755 <rohit.k09022002@gmail.com>
2025-05-17 21:58:20 +0530

    you

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rohit21755/goredis
 
 go 1.24.2
+
+require github.com/tidwall/resp v0.1.1 // indirect
--- proto.go
+++ proto.go
@@ -1,8 +1,36 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+
+	"github.com/tidwall/resp"
+)
+
 type Command struct {
 }
 
-func parseCommand(msg []byte) (Command, error) {
+type SetCommand struct {
+	key, val string
+}
+
+func parseCommand(raw string) (Command, error) {
+	rd := resp.NewReader(bytes.NewBufferString(raw))
+	for {
+		v, _, err := rd.ReadValue()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		if v.Type() == resp.Array {
+			for i, v := range v.Array() {
+				fmt.Printf("  #%d %s, value: '%s'\n", i, v.Type(), v)
+			}
+		}
+	}
 	return Command{}, nil
 }
